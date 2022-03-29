b16bc8132860cee37050fb08636ee8c2e1853c55
Tyler Harpool <THarpool@jhacorp.com>
2022-03-29 14:01:10 -0500

    Initial

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,40 @@
+import { Platform, StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+import { WebView } from 'react-native-webview';
+
+
+
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      { Platform.OS === 'ios' ? <WebView
+        originWhitelist={['*']}
+        source={{ html: '<h1>Hello world!</h1>' }}
+      /> : <h1>Hello World!</h1> }
+ 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
